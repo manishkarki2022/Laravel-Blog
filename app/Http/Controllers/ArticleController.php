62ebca2a358dc395972703fad671b739e972d944
manishkarki2022 <np03cs4s210031@heraldcollege.edu.np>
2024-02-06 15:31:44 +0545

    adjust the code of tailwind.config.js and write code of index of article for view

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        $articles = Article::with(['user','tags'])->latest()->paginate(10);
+        return view('articles.index',compact('articles'));
+
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Fig tree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
