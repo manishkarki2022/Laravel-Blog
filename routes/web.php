782ec4603de44c375c601441ae94a38735ba7d29
manishkarki2022 <np03cs4s210031@heraldcollege.edu.np>
2024-02-06 14:58:31 +0545

    Setting up Article Routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,8 +13,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::resource('/articles', ArticleController::class)->only(['index', 'show']);
 
 Route::middleware('auth')->group(function () {
+    Route::resource('/dashboard/articles', ArticleController::class)->except(['index', 'show']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
